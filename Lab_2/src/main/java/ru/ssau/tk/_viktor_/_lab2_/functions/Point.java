42f475c1fbdd26c3fb887eba71be5376a0f00ae0
ViktorBrusnikin <viktorbrusnikin35@gmail.com>
2024-11-29 20:44:48 +0400

    lab_4 2/10

---
+++ Lab_2/src/main/java/ru/ssau/tk/_viktor_/_lab2_/functions/Point.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk._viktor_._lab2_.functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+}
