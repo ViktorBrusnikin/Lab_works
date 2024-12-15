df634ad66f4e989f5a64df607209399a327cfd69
ViktorBrusnikin <viktorbrusnikin35@gmail.com>
2024-12-15 12:12:11 +0400

    lab_6 2/9 1.0

---
+++ Lab_2/src/main/java/ru/ssau/tk/_viktor_/_lab2_/concurrent/ReadTask.java
@@ -0,0 +1,18 @@
+package ru.ssau.tk._viktor_._lab2_.concurrent;
+
+import ru.ssau.tk._viktor_._lab2_.functions.TabulatedFunction;
+
+public class ReadTask implements Runnable{
+    private TabulatedFunction tabulatedFunction;
+
+    ReadTask(TabulatedFunction tabulatedFunction){
+        this.tabulatedFunction = tabulatedFunction;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < tabulatedFunction.getCount(); i++){
+            System.out.printf("After read: i = %d, x = %f, y = %f%n", i, this.tabulatedFunction.getX(i), this.tabulatedFunction.getY(i));
+        }
+    }
+}
